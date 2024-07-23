a9fc939ee8be95675defd1aebcf4d7971c39f6bc
Andrew Yupin <brolo1341@gmail.com>
2024-07-23 09:45:46 +0300

    implemented direction switcher, removed unused code, the best names are set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,15 @@ import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
-import { CoreComponent } from './core/components/core/core.component';
+import { StepperComponent } from './core/components/stepper/stepper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CoreComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, StepperComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'test-task-app';
-
-  public promptEvent: any;
-
-  @HostListener('window:beforeinstallprompt', ['$event'])
-  onbeforeinstallprompt(e: Event) {
-    e.preventDefault();
-    this.promptEvent = e;
-  }
-
-  public installPWA() {
-    this.promptEvent.prompt();
-  }
-
-  public shouldInstall(): boolean {
-    return !this.isRunningStandalone() && this.promptEvent;
-  }
-
-  public isRunningStandalone(): boolean {
-    return (window.matchMedia('(display-mode: standalone)').matches);
-  }
 }
--- src/app/core/components/stepper/stepper.component.ts
+++ src/app/core/components/stepper/stepper.component.ts
@@ -8,7 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AsyncPipe } from '@angular/common';
-import { Directionality, Direction  } from '@angular/cdk/bidi';
+import { Directionality, Direction } from '@angular/cdk/bidi';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-core',
@@ -19,11 +20,13 @@ import { Directionality, Direction  } from '@angular/cdk/bidi';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    AsyncPipe,],
-  templateUrl: './core.component.html',
-  styleUrl: './core.component.scss'
+    AsyncPipe,
+    MatSlideToggleModule,
+  ],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.scss'
 })
-export class CoreComponent {
+export class StepperComponent {
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
@@ -33,14 +36,22 @@ export class CoreComponent {
   thirdFormGroup = this._formBuilder.group({
     thirdCtrl: ['', Validators.required],
   });
-  isLinear = true;
-  direction: Direction  = 'rtl';
+
+
+  public direction: Direction = 'rtl';
+  public isRtl: boolean = true;
 
   stepperOrientation!: Observable<StepperOrientation>;
 
   constructor(private _formBuilder: FormBuilder, private dir: Directionality, breakpointObserver: BreakpointObserver,) {
+    this.isRtl = dir.value === 'rtl';
+    
     this.stepperOrientation = breakpointObserver
       .observe('(min-width: 800px)')
       .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
+
+  public toggleDirection(event: MatSlideToggleChange ): void {
+    this.direction = event.checked ? 'rtl' : 'ltr';
   }
 }
