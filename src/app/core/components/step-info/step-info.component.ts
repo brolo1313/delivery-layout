3aba7f0a680a0bb07070ce9f3e8b629280c2dcc3
Den <saloed2206@gmail.com>
2024-07-23 11:56:45 +0300

    added step component

---
+++ src/app/core/components/step-info/step-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './step-info.component.html',
+  styleUrl: './step-info.component.scss'
+})
+export class StepInfoComponent {
+
+}
