ff73512cabb4d16d68c0c18ba3731ab65b30c62b
Dmytro Parkhomenko <dmytro.parkhomenko@intellias.com>
2024-07-24 12:54:16 +0300

    stepper part 2

--- src/app/container/order/container-order-waste-contractor/container-order-waste-contractor.component.ts
+++ src/app/container/order/container-order-waste-contractor/container-order-waste-contractor.component.ts
@@ -1,13 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import {AsyncPipe} from "@angular/common";
-import {MatButton} from "@angular/material/button";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} from "@angular/material/stepper";
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
-import {map, Observable} from "rxjs";
-import {StepperOrientation} from "@angular/cdk/stepper";
-import {BreakpointObserver} from "@angular/cdk/layout";
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
+import { map, Observable } from "rxjs";
+import { StepperOrientation } from "@angular/cdk/stepper";
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-container-order-waste-contractor',
@@ -17,70 +13,66 @@ import {BreakpointObserver} from "@angular/cdk/layout";
   encapsulation: ViewEncapsulation.None
 })
 export class ContainerOrderWasteContractorComponent {
+  @ViewChild('stepper') stepper?: MatStepper;
 
   selectedIndex: number = 0;
-
   steps = [
     {
       label: 'הזמנת מכולה',
       subLabel: '(בתהליך)',
-      icon: './icons/stepper/car.svg',
+      icon: './assets/icons/stepper/car.svg',
       state: 'car',
       completed: false,
     },
     {
       label: 'תעודת הזמנה',
       subLabel: '(טרם בוצע)',
-      icon: './icons/stepper/list.svg',
+      icon: './assets/icons/stepper/list.svg',
       state: 'list',
       completed: false,
     },
     {
       label: 'תעודת איסוף',
       subLabel: '(טרם בוצע)',
-      icon: './icons/stepper/delivery.svg',
+      icon: './assets/icons/stepper/delivery.svg',
       state: 'delivery',
       completed: false,
     },
     {
       label: 'תעודת הטמנה',
       subLabel: '(טרם בוצע)',
-      icon: './icons/stepper/complete.svg',
+      icon: './assets/icons/stepper/complete.svg',
       state: 'complete',
       completed: false,
     }
   ];
 
-  setSelectedIndex(index: number) {
-    this.selectedIndex = index;
-  }
-
-  isStepCompleted(stepIndex: number): boolean {
-    console.log(stepIndex);
-    
-    return this.steps[stepIndex].completed;
-  }
-
-  completeStep(stepIndex: number): void {
-    this.steps[stepIndex].completed = true;
-  }
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required],
-  });
   stepperOrientation: Observable<StepperOrientation>;
 
   constructor(
-    private _formBuilder: FormBuilder,
-    breakpointObserver: BreakpointObserver,
+    private breakpointObserver: BreakpointObserver,
   ) {
     this.stepperOrientation = breakpointObserver
       .observe('(min-width: 800px)')
-      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
+
+  public setSelectedIndex(index: number): void {
+    this.selectedIndex = index;
+  }
+
+  public isStepCompleted(stepIndex: number): boolean {
+    return this.steps[stepIndex].completed;
+  }
+
+  public completeStep(stepIndex: number): void {
+    this.steps[stepIndex].completed = true;
+  }
+
+  public submitStep(stepInfo: any, stepIndex: number) {
+    this.completeStep(stepIndex);
+    if (stepIndex < this.steps.length - 1) {
+      this.stepper?.next();
+    }
   }
 }
